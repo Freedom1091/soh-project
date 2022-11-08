61d7f40b0408e433f94eacd521a1387562a89cf1
Freedom1091 <1396747353@qq.com>
2022-11-08 17:30:56 +0800

    03-Home界面静态资源的搭建

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,12 +1,35 @@
 <template>
   <div>
     <TypeNav />
+    <ListContainer />
+    <RecommEnd />
+    <Rank />
+    <Like />
+    <Floor />
+    <Floor />
+    <Brand />
   </div>
 </template>
 
 <script>
+// 引入组件
+import ListContainer from '@/views/Home/ListContainer'
+import RecommEnd from '@/views/Home/Recommend'
+import Rank from '@/views/Home/Rank'
+import Like from '@/views/Home/Like'
+import Floor from '@/views/Home/Floor'
+import Brand from '@/views/Home/Brand'
 export default {
-  name: 'my-home'
+  name: 'my-home',
+  // 注册组件
+  components: {
+    ListContainer,
+    RecommEnd,
+    Rank,
+    Like,
+    Floor,
+    Brand
+  }
 }
 </script>
 
